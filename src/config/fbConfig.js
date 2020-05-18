2d7b9f3b0e0cf409528e2203d16be1dc8e279e0b
dias.baiboldy <dias.baiboldy@BAIBOLDY>
2020-05-18 21:48:42 +0600

    changed

---
+++ src/config/fbConfig.js
@@ -0,0 +1,12 @@
+// var firebaseConfig = {
+//     apiKey: "AIzaSyB--7j6vKP6DRF0sJxPSgcFFmAtPyma6b8",
+//     authDomain: "mario-94e4b.firebaseapp.com",
+//     databaseURL: "https://mario-94e4b.firebaseio.com",
+//     projectId: "mario-94e4b",
+//     storageBucket: "mario-94e4b.appspot.com",
+//     messagingSenderId: "508101553369",
+//     appId: "1:508101553369:web:469b70de8b0ba2565e62c8",
+//     measurementId: "G-GSLDFWSLQD"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
